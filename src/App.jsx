94f3302a88c886e87c6d5fbdad75f9f4b2c51d39
iamprashu <iamprashu.gs@gmail.com>
2025-04-07 12:22:59 +0530

    App Component Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import PanelHeader from "./Components/PanelHeader.jsx";
 import Panel from "./Components/Panel.jsx";
+import {ToastContainer} from "react-toastify";
 
 export  default function App() {
     return(
-        <div className="App h-screen bg-yellow-50 flex flex-col justify-arround items-center">
+        <div className=" min-h-screen max bg-yellow-50 flex flex-col justify-arround items-center">
             <PanelHeader/>
             <Panel/>
+            <ToastContainer position="top-left" autoClose={1000} theme="dark"/>
         </div>
     )
 }
