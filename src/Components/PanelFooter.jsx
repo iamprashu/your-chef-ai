f147381a24daad5ae339579b7845b8fadfc8f684
iamprashu <iamprashu.gs@gmail.com>
2025-04-17 19:22:17 +0530

    Updated Project

--- src/Components/PanelFooter.jsx
+++
@@ -1,8 +0,0 @@
-export default function PanelFooter(){
-  return (
-    <div className="bg-gray-600 w-full h-[100px] rounded-b-2xl p-5 "> 
-      <p>Have you done adding items you have and need a recepie ?</p>
-      <button className="bg-gray-600 px-7 py-2 rounded-2xl hover:bg-gray-800"> Get Recepie </button>
-    </div>
-  )
-}
---
+++ src/Components/Recepie.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Recepie() {
+  return (
+    <div className='bg-red-900 min-h-[60%]'>
+      
+    </div>
+  )
+}
+
+export default Recepie
