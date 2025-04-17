61009a965337c1346fcb181f522031af03c79d72
iamprashu <iamprashu.gs@gmail.com>
2025-04-17 19:19:20 +0530

    App Component and Vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import PanelHeader from "./Components/PanelHeader.jsx";
 import Panel from "./Components/Panel.jsx";
-import {ToastContainer} from "react-toastify";
+import {Toaster} from "react-hot-toast";
+import Recepie from "./Components/Recepie.jsx";
 
 export  default function App() {
     return(
-        <div className=" min-h-screen max bg-yellow-50 flex flex-col justify-arround items-center">
+        <div className="h-screen justify-arround items-center w-full">
+            <Toaster position="top-right" autoClose={1000} theme="light"/>
             <PanelHeader/>
-            <Panel/>
-            <ToastContainer position="top-left" autoClose={1000} theme="dark"/>
+            <div className="flex flex-col md:flex-row h-[90%] md:min-h-[90%]">
+                <Panel/>
+                <Recepie/>
+            </div>
         </div>
     )
 }
