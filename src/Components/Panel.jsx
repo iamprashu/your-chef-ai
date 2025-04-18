b718a8b9798e3d875e3135af0f71ba10a6d06273
iamprashu <iamprashu.gs@gmail.com>
2025-04-18 11:59:57 +0530

    Panel Component updated-iu99

--- src/Components/Panel.jsx
+++ src/Components/Panel.jsx
@@ -1,42 +1,28 @@
-import { useState } from "react"
+import { useContext, useState } from "react"
 import {toast} from "react-hot-toast";
 import CallAi from "./CallAi.jsx";
+import { ApplicationContext } from "../Contexts/ApplicationContext.jsx";
 
 export default function Panel(){
-    const[count,setCount] = useState(0);
-    const[itemsInHand,setItemsInHand] = useState('')
-    const[itemList,setItemList] = useState([]);
-
-    function DeleteItemHandler(Updateditems){
-        setItemsInHand(Updateditems);//updating items
-        setCount(count-1)
-        setItemsInHand('')
-    }
-    function addItem(event){
-        event.preventDefault()
-        if(!itemsInHand){
-            // alert('Please Enter Item Name')
-            toast.error('Hey!, Please Enter Name of Item.')
-        }else {
-            setCount(count+1);
-            setItemList((oldItems)=>[...oldItems,itemsInHand])
-            setItemsInHand('')
-        }
-    }
+    const{itemsInHand,setItemsInHand,itemList,setItemList,count,addItem,DeleteItemHandler} = useContext(ApplicationContext);
+   
     return(
-      <div className="md:w-[40%] min-h-[40%] flex flex-col p-2 gap-5 items-center bg-gray-600 text-black">
-            <h1 className="text-xl text-black">Please enter at least 4 items</h1>
+      <div className="w-full h-[50%] flex flex-col p-2 gap-5 items-center bg-gray-600  ">
+           
 
-            <form onSubmit={addItem} className='flex gap-5'>
-              <input type="text" placeholder="Enter Items You Have...." className="bg-gray-700 md:w-60 h-10 rounded-2xl p-2 text-white" value={itemsInHand} onChange={(event)=>{setItemsInHand(event.target.value)}}></input>
-                <button className="bg-gray-700 px-5 py-2 rounded-xl text-white hover:bg-gray-600 text-sm">Add Item</button>
+            <form onSubmit={addItem} className='w-full  flex flex-col gap-2 items-center justify-evenly md:w-[60%]'>
+                <h1 className="md:text-xl ">Please enter at least 4 items</h1>
+
+              <input type="text" placeholder="Enter Items You Have...." className="bg-gray-700 md:w-2/4 w-3/4 h-10 rounded-2xl p-2 text-white" value={itemsInHand} onChange={(event)=>{setItemsInHand(event.target.value)}}></input>
+
+                <button className="bg-gray-700 px-5 py-2 rounded-xl text-white hover:bg-gray-800 text-sm">Add Item</button>
+
             </form>
-
-            <h1 className="text-xl text-black">You Have {count} items in you hand:</h1>
-          <ol className="text-xl list-decimal">
-              {itemList.map((item,index)=>{return (<li key={index}>{item}</li>)})}
-          </ol>
-          {/*  Conditional Render Heree*/}
+           <div className="w-full  flex flex-col gap-2 items-center justify-evenly md:w-[60%]">
+           <h1 className="md:text-xl ">You Have {count} items in you hand:</h1>
+           <p className="md:text-xl">{itemList.map((item,index)=>{return (<span key={index}>{index+1}. {item}{index !== (itemList.length - 1) ? ', ' : ''} </span>)})}</p>
+           </div>
+        
           <CallAi itemList={itemList} DeleteItemHandler = {DeleteItemHandler} count={count}/>
       </div>)
 }
