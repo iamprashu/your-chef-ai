32d7fc7ce779a25a5726e66366f918cb2d27a3e4
iamprashu <iamprashu.gs@gmail.com>
2025-04-19 04:07:20 +0530

    Advanced Loader

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -4,18 +4,10 @@ import { GiCampCookingPot } from "react-icons/gi";
 
 function Loader() {
   return (
-    <div className='min-h-[90%] flex flex-col justify-evenly items-center'>
+    <div className='min-h-[90%] flex flex-col justify-evenly items-center gap-12'>
        <p className='md:text-xl'>Please Wait we are Loading Recepie.....</p>
-       <div role="status" className=" animate-pulse ">
-     
-            {/* <div className="h-5.5 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-4"></div>
-            <div className="h-3 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
-            <div className="h-3 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
-            <div className="h-3 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
-            <div className="h-3 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px] mb-2.5"></div>
-            <div className="h-3 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div> */}
+       <div role="status" className=" animate-bounce "> 
             <GiCampCookingPot  size={200}/>
-            
     </div>
     </div>
     
