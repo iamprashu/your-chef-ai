66fd368c52899c0e3c39625bf35e7db4cfedccc6
iamprashu <iamprashu.gs@gmail.com>
2025-04-19 04:06:06 +0530

    App Component Updated --added warning ip log

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import PanelHeader from "./Components/PanelHeader.jsx";
 import Panel from "./Components/Panel.jsx";
 import {Toaster} from "react-hot-toast";
 import Recepie from "./Components/Recepie.jsx";
+import Warn from "./Components/Warn.jsx";
 
 export  default function App() {
     return(
         <div className="h-screen justify-arround items-center w-full bg-gray-600">
+            <Warn/>
             <Toaster position="top-center" theme="light" toastOptions={{
     className: '',
     duration: 1000,
